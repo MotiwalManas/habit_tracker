2bf84bcd37595c86428c8d38e6030e4763d7912d
Manas <123235470+MotiwalManas@users.noreply.github.com>
2023-04-24 10:35:16 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const passport = require("passport");
+
+const router = express.Router();
+
+// console.log('router is loaded');
+
+//controller for routing
+const homeController = require("../controllers/home_controller");
+
+router.get("/", passport.checkAuthentication, homeController.home);
+
+// router.get('/' , homeController.home);
+router.use("/users", require("./users"));
+
+module.exports = router;
